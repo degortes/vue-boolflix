eba277a99bc7656f02ff0c7d29caa268ebd62192
Danilo <danilodegortes@gmail.com>
2020-11-24 14:58:38 +0100

    milestone 1 ok

---
+++ main.js
@@ -0,0 +1,48 @@
+var app = new Vue ({
+    el: '#root',
+    data: {
+        search: '',
+        films: []
+    },
+    methods: {
+        searchFilm() {
+            let currentsearch = this.search;
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '56a6519b540fc03f31a569d6c934a815',
+                    query: currentsearch,
+                    language: 'it'
+                }
+            })
+            .then((risposta) => {
+                this.films = risposta.data.results
+                console.log(this.films);
+            });
+        }
+    },
+    mounted() {
+
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '56a6519b540fc03f31a569d6c934a815',
+                    query: 'fight',
+                    language: 'it'
+                }
+            })
+            .then((risposta) => {
+                this.films = risposta.data.results
+            });
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+});
