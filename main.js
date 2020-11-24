67b83105bd7a0158f676358c22a7e8d3d33b5d08
Danilo <danilodegortes@gmail.com>
2020-11-24 17:21:12 +0100

    chiamate ajax adesso sincronizzate

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var app = new Vue ({
     data: {
         search: '',
         films: [],
+        //elementi series e allshow create solo nel caso ci sia bisogno di filtrare gli array in futuro.
+        series: [],
+        allshow: [],
         languages: ['it','fr','de','ja','en','es']
     },
     methods: {
@@ -15,23 +18,23 @@ var app = new Vue ({
                     language: 'it'
                 }
             })
-            .then((risposta) => {
-                this.films = risposta.data.results
-                console.log(this.films);
+            .then((filmreply) => {
+                this.films = filmreply.data.results
+                //creata inception in modo da avere tutti i risultati solo quando disponibili.
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        api_key: '56a6519b540fc03f31a569d6c934a815',
+                        query: currentsearch,
+                        language: 'it'
+                    }
+                })
+                .then((tvreply) => {
+                    this.series = tvreply.data.results;
+                    this.allshow = [...this.films,...this.series];
+                });
+
             });
 
-            axios.get('https://api.themoviedb.org/3/search/tv', {
-                params: {
-                    api_key: '56a6519b540fc03f31a569d6c934a815',
-                    query: currentsearch,
-                    language: 'it'
-                }
-            })
-            .then((risposta) => {
-                risposta.data.results.forEach((item, i) => {
-                    this.films.push(item)
-                });
-            });
 
         }
     }
