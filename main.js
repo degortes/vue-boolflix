9a58c9be8d84f8e272fe8f2dfecdf474497753d2
Danilo <danilodegortes@gmail.com>
2020-11-25 12:59:20 +0100

    migliorate eccezioni e v-for su star

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var app = new Vue ({
     el: '#root',
     data: {
+        results: "",
+        isLoading: false,
         mySelection: [],
         search: '',
         films: [],
@@ -11,32 +13,41 @@ var app = new Vue ({
     },
     methods: {
         searchFilm() {
-            let currentsearch = this.search;
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: '56a6519b540fc03f31a569d6c934a815',
-                    query: currentsearch,
-                    language: 'it'
-                }
-            })
-            .then((filmreply) => {
-                this.films = filmreply.data.results
-                //creata inception in modo da avere tutti i risultati solo quando disponibili.
-                axios.get('https://api.themoviedb.org/3/search/tv', {
+
+            if (this.search.trim()) {
+                this.isLoading = true;
+
+                let currentsearch = this.search;
+                axios.get('https://api.themoviedb.org/3/search/movie', {
                     params: {
                         api_key: '56a6519b540fc03f31a569d6c934a815',
                         query: currentsearch,
                         language: 'it'
                     }
                 })
-                .then((tvreply) => {
-                    this.series = tvreply.data.results;
-                    this.allshow = [...this.films,...this.series];
-                    this.mySelection = this.allshow;
+                .then((filmreply) => {
+                    this.films = filmreply.data.results
+                    //creata inception in modo da avere tutti i risultati solo quando disponibili.
+                    axios.get('https://api.themoviedb.org/3/search/tv', {
+                        params: {
+                            api_key: '56a6519b540fc03f31a569d6c934a815',
+                            query: currentsearch,
+                            language: 'it'
+                        }
+                    })
+                    .then((tvreply) => {
+                        this.series = tvreply.data.results;
+                        this.allshow = [...this.films,...this.series];
+                        this.mySelection = this.allshow;
+                        this.isLoading = false;
+                    });
+
 
                 });
 
-            });
+                this.results = this.search;
+                this.search ="";
+            }
         }
     }
 
